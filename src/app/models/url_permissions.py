d2d90125d2a011174cdac459f36d006a6382e066
kirara mimi <237s10mimi@gmail.com>
2024-10-14 23:59:59 +0900

    feat: add the url_permissions model.

---
+++ src/app/models/url_permissions.py
@@ -0,0 +1,24 @@
+from django.db import models
+from app.models.roles import Roles
+from app.models.urls import Urls
+
+class Url_permissions(models.Model):
+    """
+    URL毎のアクセス権管理テーブル
+
+    Attributes:
+        id (AutoField): Auto increment ID
+        role_id (UUIDField): そのURLにアクセスできるロールのID
+        url_id (IntegerField): 対象ロールがアクセスできるURLのレコードのID
+        created_date (DateTimeField): レコード作成時間
+        updated_date (DateTimeField): レコード更新時間
+    """
+    id = models.AutoField(primary_key=True, db_column='id')
+    role_id = models.ForeignKey(Roles, on_delete=models.CASCADE, db_column='role_id', default='')
+    url_id = models.ManyToManyField(Urls, on_delete=models.CASCADE, db_column='url_id', default='')
+    created_date = models.DateTimeField(db_column='created_date', auto_now_add=True)
+    updated_date = models.DateTimeField(db_column='updated_date', auto_now=True)
+
+    class Meta:
+        # app_label = 'auth_role_paths'
+        db_table = 'url_permissions'

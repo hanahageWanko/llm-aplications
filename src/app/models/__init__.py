287b44d3609a5ca6dc601dc6f64d3dee8cc37c60
kirara mimi <237s10mimi@gmail.com>
2024-10-24 23:59:59 +0900

    fix: Changed the import name in the models __init__.py file.

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,9 +1,9 @@
 # app.models配下に設置したファイルをimportする
-from .interviews import Interviews
-from .missions import Missions
-from .personas import Personas
-from .reports import Reports
-from .role_users import Role_users
-from .roles import Roles
-from .url_permissions import Url_permissions
-from .users import Users
+from .interviews import *
+from .missions import *
+from .personas import *
+from .reports import *
+from .role_users import *
+from .roles import *
+from .url_permissions import *
+from .users import *

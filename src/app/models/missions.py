8e1abcefe9df21a48c31723ea7990607566bc94f
kirara mimi <237s10mimi@gmail.com>
2024-11-13 23:59:59 +0900

    style: formatting mission models

--- src/app/models/missions.py
+++ src/app/models/missions.py
@@ -15,11 +15,36 @@ class Missions(models.Model):
         updated_date (DateTimeField): レコード更新時間
     """
     id = models.AutoField(primary_key=True, db_column='id')
-    user_uuid = models.ForeignKey(Users, on_delete=models.CASCADE, db_column='user_uuid', default='')
-    persona_id = models.ForeignKey(Personas, on_delete=models.CASCADE, db_column='persona_id', default='' )
-    message = models.CharField(max_length=256, db_column='message', default='' )
-    created_date = models.DateTimeField(db_column='created_date', auto_now_add=True)
-    updated_date = models.DateTimeField(db_column='updated_date', auto_now=True)
+
+    user_uuid = models.ForeignKey(
+        Users, 
+        on_delete=models.CASCADE, 
+        db_column='user_uuid', 
+        default=''
+    )
+
+    persona_id = models.ForeignKey(
+        Personas, 
+        on_delete=models.CASCADE, 
+        db_column='persona_id', 
+        default='' 
+    )
+
+    message = models.CharField(
+        max_length=256, 
+        db_column='message', 
+        default='' 
+    )
+
+    created_date = models.DateTimeField(
+        db_column='created_date', 
+        auto_now_add=True
+    )
+
+    updated_date = models.DateTimeField(
+        db_column='updated_date', 
+        auto_now=True
+    )
 
     class Meta:
         # app_label = 'auth_role_paths'

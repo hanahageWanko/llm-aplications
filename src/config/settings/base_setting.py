eba32957efa48abb4eb820fee80f7bb51ccae694
kirara mimi <237s10mimi@gmail.com>
2024-12-04 23:59:59 +0900

    setting: tuning timezone & language code

--- src/config/settings/base_setting.py
+++ src/config/settings/base_setting.py
@@ -124,13 +124,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
